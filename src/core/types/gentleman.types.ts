157e64289c3dbbe6fb0e48382df03bd0f71006d3
Miguel <99726342+mig-code@users.noreply.github.com>
2022-12-11 13:38:22 +0100

    Move App component and model data

---
+++ src/core/types/gentleman.types.ts
@@ -0,0 +1,10 @@
+export type GentlemanItem = {
+    id: number;
+    name: string;
+    profession: string;
+    twitter: string;
+    picture: string;
+    alternativeText: string;
+    selected: boolean;
+};
+export type GentlemanItems = Array<GentlemanItem>;
