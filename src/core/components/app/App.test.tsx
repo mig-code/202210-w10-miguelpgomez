5d59feccb67616bfcc379c06f1064592874f8435
Miguel <99726342+mig-code@users.noreply.github.com>
2022-12-11 13:58:30 +0100

    Update App.test

--- src/core/components/app/App.test.tsx
+++ src/core/components/app/App.test.tsx
@@ -5,6 +5,6 @@ import App from './App';
 
 test('renders learn react link', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText(/The pointing gentlemen/i);
     expect(linkElement).toBeInTheDocument();
 });
