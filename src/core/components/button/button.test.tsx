9255a2aaca37d2c3b79c6091ed5f3847f4276c94
Miguel <99726342+mig-code@users.noreply.github.com>
2022-12-12 17:04:46 +0100

    bugfix indent size

--- src/core/components/button/button.test.tsx
+++ src/core/components/button/button.test.tsx
@@ -4,13 +4,20 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { Button } from './buton';
 
 describe('Given Button component', () => {
-    const buttonClassName = 'icon gentleman__icon gentleman__icon--delete fas fa-times';
+    const buttonClassName =
+        'icon gentleman__icon gentleman__icon--delete fas fa-times';
     const handleClick = jest.fn();
     const id = 1;
     test('renders a button', () => {
-        render(<Button buttonClassName={buttonClassName}handleClick={handleClick} id={id} />);
+        render(
+            <Button
+                buttonClassName={buttonClassName}
+                handleClick={handleClick}
+                id={id}
+            />
+        );
         const button = screen.getAllByTestId('btn-test');
-        
-       fireEvent.click(button[0]);
+
+        fireEvent.click(button[0]);
     });
 });
