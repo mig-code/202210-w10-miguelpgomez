03ae338dc2934671e0581e9d0c2f4e460b41f07e
Miguel <99726342+mig-code@users.noreply.github.com>
2022-12-12 10:44:26 +0100

    Divide app in components

---
+++ src/core/components/layout/layout.tsx
@@ -0,0 +1,18 @@
+import { Header } from "../header/header";
+
+
+export function Layout({
+   
+    children,
+}: {
+    
+    children: JSX.Element;
+}) {
+    return (
+        <div className="container">
+            <Header></Header>
+           {children}
+            
+        </div>
+    );
+}
